7d360dffa7761c5547aab40dded78503ad73d766
babitayogender-prog <babitayogender@gmail.com>
2026-07-11 08:31:12 +0530

    Add SZGameMode class definition

---
+++ .github/workflows/Source/SpecimenZero/Public/AI/Source/SpecimenZero/Private/AI/Source/SpecimenZero/Private/Weapon/Source/SpecimenZero/Public/Weapon/Source/SpecimenZero/Private/Weapon/Source/SpecimenZero/Public/Weapon/Source/SpecimenZero/Public/Core/SZGameMode.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SZGameMode.generated.h"
+
+UCLASS()
+class SPECIMENZERO_API ASZGameMode : public AGameModeBase
+{
+    GENERATED_BODY()
+
+public:
+    ASZGameMode();
+};
